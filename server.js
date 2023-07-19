2c4fff5e472590b7bace06ba6a964b0372fc9d19
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-19 17:10:28 +0530

    adding all the data

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log('Express server started on http://localhost:3000');
+  });
+});
